5167f7005e8cb6532ee219166d73ed2682218a9a
Rylan Dalpez <rylandalpez@Rylans-MacBook-Air-2.local>
2024-09-11 03:02:32 -0700

    Standardize size of grid.

--- script.js
+++ script.js
@@ -5,19 +5,29 @@ const button = document.createElement("button");
 button.innerText = "Create New Grid";
 button.addEventListener("click", () => {
     let input = "";
-    input = prompt("Input the number of sides for the grid");
+    input = prompt("Input the number of sides for the grid (1-100)");
     removeGrid();
     generateGrid(input);
 })
 buttonContainer.appendChild(button);
 
 function generateGrid(dimensions) {
+    if (dimensions < 1)
+        dimensions = 1;
+    if (dimensions > 100)
+        dimensions = 100;
+
+    const sizing = (680 / dimensions) - 1;
+    console.log(sizing);
+
     for (let i = 0; i < dimensions; i++) {
         const div = document.createElement("div");
         div.classList.add("row");
         for (let j = 0; j < dimensions; j++) {
             const div2 = document.createElement("div");
             div2.classList.add("grid-box");
+            div2.style.width = sizing + "px";
+            div2.style.height = sizing + "px";
             div.appendChild(div2);
         }
         gridContainer.appendChild(div);
