ecb810e59855046e84e1e00651d77bb768e280e4
Rylan Dalpez <rylandalpez@Rylans-MacBook-Air-2.local>
2024-09-11 02:36:29 -0700

    Add grid styling and revamp grid generation.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement("div");
-    div.className = "row";
-    for (let j = 0; j < 16; j++) {
-        const div2 = document.createElement("div");
-        div2.textContent = "#";
-        div.appendChild(div2);
+function generateGrid(dimensions) {
+    for (let i = 0; i < dimensions; i++) {
+        const div = document.createElement("div");
+        div.classList.add("row");
+        for (let j = 0; j < dimensions; j++) {
+            const div2 = document.createElement("div");
+            div2.classList.add("grid-box");
+            div.appendChild(div2);
+        }
+        container.appendChild(div);
     }
-    container.appendChild(div);
+}
+
+generateGrid(16);
+
+const gridBoxes = Array.from(document.querySelectorAll(".grid-box"));
+for (const box of gridBoxes) {
+    box.addEventListener("mouseover", () => {
+        box.classList.add("enabled");
+    })
 }
